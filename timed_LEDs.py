ec67d2454f933370e26863163e38704310f463f3
JordanComstock <jpc2425@rit.edu>
2018-03-27 10:32:14 -0400

    Integrated timing code and pool slope code

--- timed_LEDs.py
+++ timed_LEDs.py
@@ -17,10 +17,10 @@ def opt_parse():
         signal.signal(signal.SIGINT, signal_handler)
 
 # Time Constant
-PACE1 = 20
-PACE2 = 15
-PACE3 = 15
-PACE4 = 10
+PACE1 = 20 - SCALER
+PACE2 = 15 - SCALER
+PACE3 = 15 - SCALER
+PACE4 = 10 - SCALER
 LAPS = 4
 SCALER = 4.5
 
@@ -37,7 +37,11 @@ LED_STRIP       = ws.WS2812_STRIP       # Specific LED strip we have
 POOL_X_VALUES = [0, 16.5, 21, 75]
 POOL_Y_VALUES = [4, 5, 6.5, 8]
 
-def pool_slope_calculations(time):
+def pool_slope_calculations(strip, numLaps, lapTimes):
+
+    for i in range(numLaps):
+        lapTimes[i] -= SCALER
+
     horizontal_length = []
     diagonal_length = []
     total_horizontal_length = 0
@@ -50,31 +54,41 @@ def pool_slope_calculations(time):
         total_horizontal_length += x_diff
         total_diagonal_length += math.sqrt(x_diff ** 2 + y_diff ** 2)
     led_in_section = []
-    time_in_section = []
-    for i in range(len(horizontal_length)):
+    for i in range(len(diagonal_length)):
         led_in_section.append(LED_COUNT * diagonal_length[i]/total_diagonal_length)
-        time_in_section.append(time * horizontal_length[i]/total_horizontal_length)
+    
+    for n in range(numLaps):  
+        for i in range(len(horizontal_length)):
+            time_in_section = lapTimes[n] * horizontal_length[i]/total_horizontal_length
+            if n % 2 == 0:
+                follow_odd(strip, time_in_section, led_in_section[i])
+            else:
+                follow_even(strip, time_in_section, led_in_section[i])
 
 '''
-odd laps first = 0 last = num pix = num pixels
-even laps first = num pixels last =0
+Odd laps 
+first = 0 
+last = numPixels
 '''
-def follow_odd(strip, pace):
-    pace = pace - SCALER
+def follow_odd(strip, pace, ledCount):
     strip.setPixelColor(0, Color(0,255,0))
     for i in range(0, strip.numPixels()-2, 2):
         strip.setPixelColor(i+2, Color(0,255,0))
         strip.show()
-        time.sleep(2*(float(pace)/LED_COUNT))
+        time.sleep(2*(float(pace)/ledCount))
         strip.setPixelColor(i, Color(0,0,0))
 
-def follow_even(strip, pace):
-    pace = pace - SCALER
-    strip.setPixelColor(LED_COUNT-2, Color(0,255,0))
+'''
+Even laps
+first = numPixels 
+last = 0
+'''
+def follow_even(strip, pace, ledCount):
+    strip.setPixelColor(strip.numPixels()-2, Color(0,255,0))
     for i in range(strip.numPixels(), 0, -2):
         strip.setPixelColor(i-2, Color(0,255,0))
         strip.show()
-        time.sleep(2*(float(pace)/LED_COUNT))
+        time.sleep(2*(float(pace)/ledCount))
         strip.setPixelColor(i, Color(0,0,0))
 
 # Main program 
@@ -85,15 +99,21 @@ if __name__ == '__main__':
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
     
     strip.begin()
-
+    
     start = time.time()
-    follow_odd(strip,PACE1)
+    pool_slope_calculations(strip, 2, [15, 20]
     end = time.time()
     print(end-start)
-    print(PACE1)
+    print(15+20)
 
-    start = time.time()
-    follow_even(strip, PACE2)
-    end = time.time()
-    print(end-start)
-    print(PACE2)
+    #start = time.time()
+    #follow_odd(strip,PACE1, strip.numPixels())
+    #end = time.time()
+    #print(end-start)
+    #print(PACE1)
+
+    #start = time.time()
+    #follow_even(strip, PACE2, strip.numPixels())
+    #end = time.time()
+    #print(end-start)
+    #print(PACE2)
